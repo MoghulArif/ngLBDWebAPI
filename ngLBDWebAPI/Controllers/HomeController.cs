bd0f33ad7995b9a5af16f351121a7ec100b57a7e
towseef85 <towseef85@gmail.com>
2018-01-24 11:48:52 +0530

    Added database model

--- ngLBDWebAPI/Controllers/HomeController.cs
+++ ngLBDWebAPI/Controllers/HomeController.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ngLBDWebAPI.Models;
 
 namespace ngLBDWebAPI.Controllers
 {
     public class HomeController : ApiController
     {
-        public string Get()
+        public IEnumerable<Register> Get()
         {
-            return "hi";
+            List<Register> regList;
+            using (var context = new RegistrationDBEntities())
+            {
+                regList = context.Registers.ToList();
+               
+            }
+            return regList;
+            
+        }
+
+        public string  Get(int i)
+        {
+            return "h1";
         }
     }
 }
