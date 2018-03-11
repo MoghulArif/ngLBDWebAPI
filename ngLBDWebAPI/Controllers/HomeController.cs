f1a2c8b30f68d5dccd76e2d11c4ea217287ef1a8
Arif <arif.ms2011@gmail.com>
2018-03-11 16:57:08 +0400

    Web API added

--- ngLBDWebAPI/Controllers/HomeController.cs
+++ ngLBDWebAPI/Controllers/HomeController.cs
@@ -19,20 +19,136 @@ namespace ngLBDWebAPI.Controllers
         //    using (var context = new RegistrationDBEntities())
         //    {
         //        regList = context.Registers.ToList();
-
+        /*
+         
+             
+             */
         //    }
         //    return regList;
 
         //}
 
-        public string Get()
+        public int Post(HomeModel homeModel)
         {
-            return "h1";
+            int id;
+            try
+            {
+                home hmeEF = new home()
+                {
+                    firstName = homeModel.firstName,
+                    Mobile = homeModel.mobile,
+                    Remarks = homeModel.remarks,
+                    Email = homeModel.email,
+                    locDubaiBusines = homeModel.locDubaiBusiness,
+                    locDubaiSilicon = homeModel.locDubaiSilicon,
+                    Budget400K700K = homeModel.budget400K700K,
+                    Budget700K1000K = homeModel.budget700K1000K,
+                    Budget1000K1250K = homeModel.budget1000K1250K,
+                    Budget1250K1500K = homeModel.budget1250K1500K,
+                    Studio = homeModel.studio,
+                    OneBed = homeModel.oneBed,
+                    TwoBed = homeModel.twoBed,
+                    Retail = homeModel.retail,
+                    Immed = homeModel.immed,
+                    Months3to6 = homeModel.months3to6,
+                    Months6to12 = homeModel.months6to12,
+                    Months12to18 = homeModel.months12to18
+                };
+                // will save data here in database;
+                using (BGEnqiryEntities context = new BGEnqiryEntities())
+                {
+                    context.homes.Add(hmeEF);
+                    context.SaveChanges();
+
+                    id = hmeEF.Id;
+
+                   
+                }
+                return id;
+
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+            
         }
 
-        public string  Get(string i)
+        public List<HomeModel> Get()
         {
-            return "h1" + i;
+            List<HomeModel> hm = new List<HomeModel>();
+            string fn = "";
+            string mob = "";
+
+            using (BGEnqiryEntities ctx = new BGEnqiryEntities())
+            {
+                //hm = (from c in ctx.homes
+                //             //join s in ctx.States
+                //             //on c.StateId equals s.StateId
+                //             //where c.Pincode == iPincode
+                //             select new
+                //             {
+                //                 c.firstName,
+                //                 c.lastName
+
+                //             }).ToList<HomeModel>();
+
+                var query = (from c in ctx.homes
+                             select c);
+
+                if(query != null)
+                {
+                    foreach(home h in query)
+                    {
+                        fn = h.firstName;
+                        mob = h.Mobile;
+                        hm.Add(new HomeModel() { firstName = fn, mobile = mob });
+                    }
+                }
+
+                return hm;
+
+             
+            }
+
+            //return "h1";
+        }
+
+        public HomeModel Get(int id) 
+        {
+            HomeModel hm = new HomeModel();
+            string fn = "";
+            string mob = "";
+
+            using (BGEnqiryEntities ctx = new BGEnqiryEntities())
+            {
+                //hm = (from c in ctx.homes
+                //             //join s in ctx.States
+                //             //on c.StateId equals s.StateId
+                //             //where c.Pincode == iPincode
+                //             select new
+                //             {
+                //                 c.firstName,
+                //                 c.lastName
+
+                //             }).ToList<HomeModel>();
+
+                var query = (from c in ctx.homes
+                             where c.Id == id
+                             select c);
+
+                if (query != null)
+                {
+                    foreach (home h in query)
+                    {
+                        fn = h.firstName;
+                        mob = h.Mobile;
+                        hm = new HomeModel() { firstName = fn, mobile = mob };
+                    }
+                }
+
+                return hm;
+            }
         }
     }
 }
---
+++ ngLBDWebAPI/Models/HomeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ngLBDWebAPI.Models
+{
+    public class HomeModel
+    {
+        public string firstName { get; set; }
+        public string mobile { get; set; }
+        public string remarks { get; set; }
+        public string email { get; set; }
+        public bool locDubaiBusiness { get; set; }
+        public bool locDubaiSilicon { get; set; }
+        public bool budget400K700K { get; set; }
+        public bool budget700K1000K { get; set; }
+        public bool budget1000K1250K { get; set; }
+        public bool budget1250K1500K { get; set; }
+        public bool studio { get; set; }
+        public bool oneBed { get; set; }
+        public bool twoBed { get; set; }
+        public bool retail { get; set; }
+        public bool immed { get; set; }
+        public bool months3to6 { get; set; }
+        public bool months6to12 { get; set; }
+        public bool months12to18 { get; set; }
+
+    }
+}
